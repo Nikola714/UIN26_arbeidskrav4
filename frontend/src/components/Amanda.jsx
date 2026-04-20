b52175ceb43149960a9b9717b994422d764bed10
Amandatorst <amanda.torstensen@gmail.com>
2026-04-20 14:57:58 +0200

    Router dom og koding på amanda

--- frontend/src/components/Amanda.jsx
+++ frontend/src/components/Amanda.jsx
@@ -1,4 +1,13 @@
+
+
 export default function amanda() {
+
     return (
         <h2>Amanda Torstensen</h2> 
+        <h3>Studie: Digitale medier og Design</h3>
+        <h4>Andreårs student</h4>
+
     )}
+
+
+    // kode for e-mail lenke: https://www.30secondsofcode.org/react/s/mailto-callto/#:~:text=This%20scheme%20allows%20you%20to%20create%20a,the%20recipient's%20email%20address%20already%20filled%20in.
