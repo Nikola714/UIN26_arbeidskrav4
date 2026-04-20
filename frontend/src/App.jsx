ea3c566ac9e8c0098cf0935ce67e69edebc279bf
tapperisa-wq <tapperisa@hotmail.com>
2026-04-20 14:11:56 +0200

    Router
    Legge inn router i app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route index element={<h1>Home</h1>} />
+    </Routes>
   )
 }
 
