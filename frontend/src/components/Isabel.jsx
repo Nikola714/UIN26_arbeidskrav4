7510b6de8fc42b15de1b84ea1c3a5e1cc2d40ffe
tapperisa-wq <tapperisa@hotmail.com>
2026-04-20 15:59:12 +0200

    Isabel
    Legge til info

--- frontend/src/components/Isabel.jsx
+++ frontend/src/components/Isabel.jsx
@@ -1,6 +1,11 @@
 export default function Isabel(){
 
     return(
-        
+    <>
+    <h1>Isabel Tapper</h1>
+    <a href="mailto:istap9887@hiof.no">istap9887@hiof.no</a>
+    <h2>Studie: Enkeltemner</h2>
+    <h4>Starter Informasjonssystemer høsten 2026</h4>
+    </>    
     )
 }
