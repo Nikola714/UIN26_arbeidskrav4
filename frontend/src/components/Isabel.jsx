b5cf269d24e2350a408907d56c67ddfe86014f59
tapperisa-wq <tapperisa@hotmail.com>
2026-04-20 16:19:30 +0200

    legge inn bilde

--- frontend/src/components/Isabel.jsx
+++ frontend/src/components/Isabel.jsx
@@ -3,6 +3,7 @@ export default function Isabel(){
     return(
     <>
     <h1>Isabel Tapper</h1>
+    <img src="../public/Bilder/isabel.JPG" alt="portrettIsabel"></img>
     <a href="mailto:istap9887@hiof.no">istap9887@hiof.no</a>
     <h2>Studie: Enkeltemner</h2>
     <h4>Starter Informasjonssystemer høsten 2026</h4>
