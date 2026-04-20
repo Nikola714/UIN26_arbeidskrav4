cc2a386cdc83e7e3fed2d130ee7ff0e4ff914a85
tapperisa-wq <tapperisa@hotmail.com>
2026-04-20 15:13:04 +0200

    Sanity

---
+++ backend/schemaTypes/arbeidskrav.js
@@ -0,0 +1,22 @@
+const arbeidskrav = {
+    name: "arbeidskrav",
+    title: "Arbeidskrav",
+    type: "document",
+    fields: [
+        {
+            name: "arbeidskravnavn",
+            title: "Navn på arbeidskrav",
+            type:"string"
+
+        },
+        {
+            name: "beskrivelse",
+            title:"Beskrivelse av arbeidskravet",
+            type:"string"
+        }
+        
+    ]
+
+}
+
+export default arbeidskrav
