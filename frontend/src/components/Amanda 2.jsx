58a0ea7c8b4d1cd56ee60b9b0dc7694625141fd4
tapperisa-wq <tapperisa@hotmail.com>
2026-04-21 08:38:34 +0200

    .

---
+++ frontend/src/components/Amanda 2.jsx
@@ -0,0 +1,9 @@
+export default function amanda() {
+
+    return (
+        <h2>Amanda Torstensen</h2> 
+        <h3>Studie: Digitale medier og Design</h3>
+        <h4>Andreårs student</h4>
+    )}
+
+
